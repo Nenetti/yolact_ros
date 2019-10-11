2aeb2e89a1db839b7275a0b88e3ddd163d47e0d9
Nenetti <akinori1090@gmail.com>
2019-10-11 15:24:07 +0900

    テスト

--- evaluation.py
+++ evaluation.py
@@ -253,8 +253,7 @@ class YolactRos:
             for j in range(num_dets_to_consider):
                 c = cfg.dataset.class_names[classes[j]]
                 if c == "refrigerator":
-                    # inv_alph_masks[j] -= 1
-                    pprint.pprint(inv_alph_masks[j])
+                    masks_color[j] *= 0
 
             # I did the math for this on pen and paper. This whole block should be equivalent to:
             #    for j in range(num_dets_to_consider):
