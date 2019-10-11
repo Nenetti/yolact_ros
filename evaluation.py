49d77ae72ad25b758dc76c9ee4c9ef27bd075b95
Nenetti <akinori1090@gmail.com>
2019-10-11 15:18:39 +0900

    テスト

--- evaluation.py
+++ evaluation.py
@@ -251,7 +251,7 @@ class YolactRos:
             inv_alph_masks = masks * (-mask_alpha) + 1
             for j in range(num_dets_to_consider):
                 c = cfg.dataset.class_names[classes[j]]
-                if c == "refrigerator":
+                if c != "refrigerator":
                     inv_alph_masks[j] = inv_alph_masks[j] * 0
 
             # I did the math for this on pen and paper. This whole block should be equivalent to:
